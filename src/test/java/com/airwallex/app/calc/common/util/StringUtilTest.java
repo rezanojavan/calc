946fafe82de29b38b609c4e60b90309788a363bb
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-19 00:03:38 +0100

    StringUtil unit test has been added

---
+++ src/test/java/com/airwallex/app/calc/common/util/StringUtilTest.java
@@ -0,0 +1,46 @@
+package com.airwallex.app.calc.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test StringUtil
+ */
+class StringUtilTest {
+
+    /**
+     * Sample string that used by tests
+     */
+    private static final String TEST_STRING = "5 2 *";
+
+    @Test
+    void splitBySpace() {
+        String[] strings = StringUtil.splitBySpace(TEST_STRING);
+        assertNotNull(strings);
+        assertEquals(3, strings.length);
+    }
+
+    @Test
+    void split() {
+        String[] strings = StringUtil.split(TEST_STRING, " ");
+        assertNotNull(strings);
+        assertEquals(3, strings.length);
+    }
+
+    @Test
+    void splitWhenNoSpaceExist() {
+        String[] strings = StringUtil.split("5", " ");
+        assertNotNull(strings);
+        assertEquals(1, strings.length);
+    }
+
+    @Test
+    void listBySpace() {
+        List<String> strings = StringUtil.listBySpace(TEST_STRING);
+        assertNotNull(strings);
+        assertEquals(3, strings.size());
+    }
+}
