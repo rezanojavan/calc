2e5c7725118b9fb3bc1ec01f2eccc7afe71d3fb5
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-18 21:55:24 +0100

    A new exception( ArithmeticException ) has added

---
+++ src/main/java/com/airwallex/app/calc/common/exception/ArithmeticException.java
@@ -0,0 +1,19 @@
+package com.airwallex.app.calc.common.exception;
+
+/**
+ * Arithmetic exception that represent an exception occurred when executing operations
+ *
+ * @author Reza Nojavan
+ * @version 1.0-SNAPSHOT
+ */
+public class ArithmeticException extends Exception {
+
+    /**
+     * Initiate exception by given message
+     *
+     * @param message to initiate exception
+     */
+    public ArithmeticException(String message) {
+        super(message);
+    }
+}
