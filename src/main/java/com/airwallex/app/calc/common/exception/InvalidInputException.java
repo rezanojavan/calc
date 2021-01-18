69ab53350fb97b0f57567b203eafc710f8bad5f3
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-18 21:53:28 +0100

    A new exception( InvalidInputException ) has added

---
+++ src/main/java/com/airwallex/app/calc/common/exception/InvalidInputException.java
@@ -0,0 +1,19 @@
+package com.airwallex.app.calc.common.exception;
+
+/**
+ * Invalid input exception that represent an invalid input found
+ *
+ * @author Reza Nojavan
+ * @version 1.0-SNAPSHOT
+ */
+public class InvalidInputException extends Exception {
+
+    /**
+     * Initiate an {@code InvalidInputException} by given message
+     *
+     * @param message exception description
+     */
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
