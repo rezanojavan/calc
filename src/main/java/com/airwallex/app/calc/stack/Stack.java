da43eb5f1c03c44ee2139fc5d0d9c30190932bdc
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-19 19:29:37 +0100

    Stack specification has added

---
+++ src/main/java/com/airwallex/app/calc/stack/Stack.java
@@ -0,0 +1,26 @@
+package com.airwallex.app.calc.stack;
+
+import org.apiguardian.api.API;
+
+/**
+ * This is stack specification that provide two functionalities Pop & Push
+ *
+ * @author Reza Nojavan
+ */
+@API(status = API.Status.EXPERIMENTAL)
+public interface Stack {
+
+    /**
+     * Pop top item of stack. If stack empty then return null
+     *
+     * @return top item of stack
+     */
+    String pop();
+
+    /**
+     * this method push new item into stack
+     *
+     * @param item to push to stack
+     */
+    void push(String item);
+}
