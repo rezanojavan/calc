a1dba16326e9c694d2c56481165f00b96ff62f76
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-19 08:54:41 +0100

    Operation executor specification has added

---
+++ src/main/java/com/airwallex/app/calc/operation/OperationExecutor.java
@@ -0,0 +1,33 @@
+package com.airwallex.app.calc.operation;
+
+import com.airwallex.app.calc.common.exception.InvalidInputException;
+import org.apiguardian.api.API;
+
+/**
+ * Operation specification that all operation should implement this specification. The implementations of this
+ * specification should be take of their object state because object instances will be share among all statements.
+ * In other word implementation should be stateless
+ *
+ * @author Reza Nojavan
+ */
+@API(status = API.Status.EXPERIMENTAL)
+public interface OperationExecutor {
+
+    /**
+     * This method execute operation and return result
+     *
+     * @param input to execute operation
+     * @return result
+     * @throws InvalidInputException if invalid input passed
+     * @throws ArithmeticException   if any error happened
+     */
+    String execute(String... input) throws InvalidInputException, ArithmeticException;
+
+    /**
+     * Get operation that this executor can execute
+     *
+     * @return operation
+     */
+    String operation();
+
+}
