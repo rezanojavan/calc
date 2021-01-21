f2b86afe76d209a966374e75c05b2765b22ecbe2
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-21 13:39:58 +0100

    Application has been added

---
+++ src/main/java/com/airwallex/app/calc/Application.java
@@ -0,0 +1,54 @@
+package com.airwallex.app.calc;
+
+import com.airwallex.app.calc.common.exception.ArithmeticException;
+import com.airwallex.app.calc.common.exception.InvalidInputException;
+import com.airwallex.app.calc.common.exception.OperationNotSupportedException;
+import com.airwallex.app.calc.common.exception.ReadException;
+import com.airwallex.app.calc.input.InputProcessor;
+import com.airwallex.app.calc.input.TerminalInputProcessor;
+import com.airwallex.app.calc.service.CalculatorService;
+import com.airwallex.app.calc.service.CalculatorServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+/**
+ * Run Calculator
+ *
+ * @author Reza Nojavan
+ * @version 1.0-SNAPSHOT
+ */
+public class Application {
+
+    private final static Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        InputProcessor inputProcessor = new TerminalInputProcessor();
+        CalculatorService calculatorService = new CalculatorServiceImpl();
+
+        String result = null;
+        List<String> inputs = null;
+        while( true) {
+            int i = 0;
+            try {
+                logger.info("Waiting for input: ");
+                inputs = inputProcessor.read();
+                for ( ; i < inputs.size(); i++) {
+                    if ( "exit".equalsIgnoreCase(inputs.get(i)) ) {
+                        logger.info("Result: {}", result);
+                        System.exit(0);
+                    }
+                    result = calculatorService.acceptInput(inputs.get(i));
+                }
+                logger.info("Result: {}", result);
+            } catch (ReadException | OperationNotSupportedException | InvalidInputException e) {
+                logger.error(e.getMessage(), e);
+            } catch (ArithmeticException e) {
+                logger.error("operator {} (position: {}): insuciant parameters", inputs.get(i), (i*2) + 1);
+                logger.info("Result: {}", result);
+            }
+        }
+    }
+
+}
