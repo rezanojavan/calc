1135b0ec64f7b6c0b878b88d1e2187763af46c01
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-21 13:25:26 +0100

    CalculatorService spec implementation has been added

---
+++ src/main/java/com/airwallex/app/calc/service/CalculatorServiceImpl.java
@@ -0,0 +1,39 @@
+package com.airwallex.app.calc.service;
+
+import com.airwallex.app.calc.common.exception.ArithmeticException;
+import com.airwallex.app.calc.common.exception.InvalidInputException;
+import com.airwallex.app.calc.common.exception.OperationNotSupportedException;
+import com.airwallex.app.calc.engine.CalculatorEngine;
+import com.airwallex.app.calc.engine.CalculatorEngineImpl;
+
+import java.util.List;
+
+/**
+ * Calculator service implementation
+ *
+ * @author Reza Nojavan
+ * @version 1.0-SNAPSHOT
+ */
+public class CalculatorServiceImpl implements CalculatorService {
+
+    private CalculatorEngine engine = new CalculatorEngineImpl();
+
+    @Override
+    public String acceptInput(String input) throws InvalidInputException, OperationNotSupportedException, ArithmeticException {
+        engine.accept(input);
+        return engine.getResult();
+    }
+
+    @Override
+    public String acceptInput(List<String> inputs) throws InvalidInputException, ArithmeticException, OperationNotSupportedException {
+        String result = "";
+        for ( int i = 0; i < inputs.size(); i++)
+            result = acceptInput(inputs.get(i));
+        return result;
+    }
+
+    @Override
+    public String getResult() {
+        return engine.getResult();
+    }
+}
