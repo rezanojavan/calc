4bf5531420fe9a9ff3baf8dab6dafa1123876bd4
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-21 13:23:52 +0100

    changing return types to String

--- src/main/java/com/airwallex/app/calc/service/CalculatorService.java
+++ src/main/java/com/airwallex/app/calc/service/CalculatorService.java
@@ -1,6 +1,8 @@
 package com.airwallex.app.calc.service;
 
+import com.airwallex.app.calc.common.exception.ArithmeticException;
 import com.airwallex.app.calc.common.exception.InvalidInputException;
+import com.airwallex.app.calc.common.exception.OperationNotSupportedException;
 import org.apiguardian.api.API;
 
 import java.util.List;
@@ -17,26 +19,28 @@ public interface CalculatorService {
      * Accept new input to calculator engine
      *
      * @param input new input
-     * @return a list of string that present latest state after input applied
+     * @return latest state after input applied
      * @throws InvalidInputException if input is not valid
      * @throws ArithmeticException   if any error happening during executing operators
+     * @throws OperationNotSupportedException if unsupported operation passed
      */
-    List<String> acceptInput(String input) throws InvalidInputException, ArithmeticException;
+    String acceptInput(String input) throws InvalidInputException, ArithmeticException, OperationNotSupportedException;
 
     /**
      * Accept new inputs to calculator engine
      *
      * @param inputs new inputs
-     * @return a list of string that present latest state after inputs applied
+     * @return latest state after inputs applied
      * @throws InvalidInputException if input is not valid
      * @throws ArithmeticException   if any error happening during executing operators
+     * @throws OperationNotSupportedException if unsupported operation passed
      */
-    List<String> acceptInput(List<String> inputs) throws InvalidInputException, ArithmeticException;
+    String acceptInput(List<String> inputs) throws InvalidInputException, ArithmeticException, OperationNotSupportedException;
 
     /**
      * Get result of calculation
      *
      * @return result
      */
-    List<String> getResult();
+    String getResult();
 }
