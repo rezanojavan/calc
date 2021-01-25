c0de21114470c39fc117906b0182da525e914e6a
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-25 08:41:59 +0100

    a new unit test for history service has been added

---
+++ src/test/java/com/airwallex/app/calc/history/StackBackendHistoryServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.airwallex.app.calc.history;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * History implementation unit test
+ *
+ * @author Reza Nojavan
+ * @version 1.0-SNAPSHOT
+ */
+class StackBackendHistoryServiceImplTest {
+
+    static StackBackendHistoryServiceImpl historyService;
+    static String[] historyRecord = new String[]{"2", "5"};
+
+    @BeforeAll
+    static void setup() {
+        historyService =  new StackBackendHistoryServiceImpl();
+    }
+
+    @Test
+    void store() {
+        historyService.store(historyRecord);
+    }
+
+    @Test
+    void retrieve() {
+        historyService.store(historyRecord);
+        String[] record = historyService.retrieve();
+        assertEquals(historyRecord, record);
+    }
+}
