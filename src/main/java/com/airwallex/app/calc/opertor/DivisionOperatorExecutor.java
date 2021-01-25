383e0d67bc6480dcdef972e714078bab80430bbf
Reza Nojavan <reza.nojavan@gmail.com>
2021-01-25 08:42:48 +0100

    convert method moved to NumberUtil

--- src/main/java/com/airwallex/app/calc/opertor/DivisionOperatorExecutor.java
+++ src/main/java/com/airwallex/app/calc/opertor/DivisionOperatorExecutor.java
@@ -14,22 +14,7 @@ public class DivisionOperatorExecutor implements OperatorExecutor {
     public String execute(String... input) throws InvalidInputException, ArithmeticException {
         if (input == null || input.length < 2)
             throw new InvalidInputException("Not enough input. At least two input should be provided");
-        return NumberUtil.format15Decimal(convert(input[0]) / convert(input[1]));
-    }
-
-    /**
-     * This method convert an string to Double
-     *
-     * @param input to convert
-     * @return converted Double
-     * @throws InvalidInputException in input is not valid
-     */
-    private Double convert(String input) throws InvalidInputException {
-        try {
-            return Double.valueOf(input);
-        } catch (NumberFormatException ex) {
-            throw new InvalidInputException(String.format("input %s is not a number", input));
-        }
+        return NumberUtil.format15Decimal(NumberUtil.convert(input[0]) / NumberUtil.convert(input[1]));
     }
 
     @Override
